63a4d10662a62a6db88051d0e295b0dab6b56bdc
Joselito2009 <jos3descargas2@gmail.com>
2025-09-08 19:14:26 -0500

    Create Name.py
    lol

---
+++ Name.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROLF": "Una respuesta a una broma",
+            "SHEESH": "Ligera desaprobación",
+            "CREEPY": "Aterrador, siniestro",
+            "AGGRO": "Ponerse agresivo/enojado",
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])# ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    print("Oops, hubo un error, inténtalo de nuevo")
+    
